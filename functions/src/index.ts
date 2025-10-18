3de327d2e3fbf3328a6e57e0bf9016d4b8cf3cb0
Valentin Peluso 2 <valentin.peluso.92@gmail.com>
2025-10-18 17:15:32 -0300

    fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,5 +28,5 @@ setGlobalOptions({maxInstances: 10});
 
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send({message: "Hello from Firebase 2!"});
 });
